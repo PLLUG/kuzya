05bf7f895ad7944d92b11444bf667c7c0fdce77d
volder <vshevchyk@users.noreply.github.com>
2009-07-06 23:27:50 +0000

    nothink to be done

--- trunc/src/ui_finddialog.h
+++ trunc/src/ui_finddialog.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading ui file 'finddialog.ui'
 **
-** Created: Tue Jul 7 02:02:07 2009
+** Created: Tue Jul 7 02:44:15 2009
 **      by: Qt User Interface Compiler version 4.5.1
 **
 ** WARNING! All changes made in this file will be lost when recompiling ui file!
