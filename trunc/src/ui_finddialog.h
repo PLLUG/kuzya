caf6a7b39b09147128b340d5cb0bf4b8ba915cd0
buoyantgrumbler <buoyantgrumbler@07ffc96b-09f0-4c3d-8597-9d5a3a30b836>
2009-07-14 23:19:46 +0000

    added check for accessible rights to save file

--- trunc/src/ui_finddialog.h
+++ trunc/src/ui_finddialog.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading ui file 'finddialog.ui'
 **
-** Created: Fri 10. Jul 20:47:34 2009
+** Created: Tue Jul 14 22:07:15 2009
 **      by: Qt User Interface Compiler version 4.5.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling ui file!
